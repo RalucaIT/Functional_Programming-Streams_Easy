b893ed684ca3f9ded20e68706a3091c5d4cc880f
rvoicu <raluca.teodora.voicu@gmail.com>
2022-06-29 17:23:47 +0300

    commited Streams_Difficult

--- src/main/java/com/homework/exercises/Pack_4_Streams_Difficult.java
+++ src/main/java/com/homework/exercises/Pack_4_Streams_Difficult.java
@@ -6,14 +6,21 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import java.text.DecimalFormat;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 import static com.shazam.shazamcrest.MatcherAssert.assertThat;
 import static com.shazam.shazamcrest.matcher.Matchers.sameBeanAs;
 import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.*;
 
 @SuppressWarnings("all")
 public class Pack_4_Streams_Difficult {
@@ -27,13 +34,14 @@ public class Pack_4_Streams_Difficult {
         // the solution has to be a single statement, complexity O(n^2) is acceptable //  2 for-uri
 
         String result = null;
+        result = EMPLOYEES.stream().filter(firstEmployee -> EMPLOYEES.stream().anyMatch(secondEmployee -> firstEmployee.getFirstName().equals(secondEmployee.getFirstName()) && firstEmployee.getSurname().equals(secondEmployee.getSurname()) && !firstEmployee.equals(secondEmployee)))
+                        .map(employee -> employee.getFirstName() + " " + employee.getSurname()).findFirst() // first True
+                        .orElse(null);
 
-        EMPLOYEES.stream().forEach(one -> {EMPLOYEES.stream();} );
-
+        System.out.println(result);
         assertThat(result, sameBeanAs("Holly Davies"));
     }
 
-    @Ignore
     @Test
     public void exercise_2_groupingBy_counting() {
         // find the total number of groups of at least 5 employees living close to each other
@@ -41,25 +49,25 @@ public class Pack_4_Streams_Difficult {
         // you can collect to map and then stream over it, however the solution has to be a single statement
 
         long result = 0;
+        result = EMPLOYEES.stream()
+                .collect(groupingBy(employees -> employees.getHomeAddress().getPostCode().substring(0, 2)))
+                        .values().stream().filter(employees -> employees.size() >= 5).count(); // count() returneaza din Lista in Long
 
-        //TODO write your code here
-
+        System.out.println(result);
         assertThat(result, sameBeanAs(110L));
     }
 
-    @Ignore
     @Test
     public void exercise_3_flatMap() {
         // find the total number of all different home and correspondence addresses
 
         long result = 0;
-
-        //TODO write your code here
-
-        assertThat(result, sameBeanAs(1820L));
+        result = EMPLOYEES.stream()
+        .flatMap(employees -> Stream.of(employees.getHomeAddress(),employees.getCorrespondenceAddress().get())).distinct().count(); // flatMap (grupeaza) creeaza o noua matrice din home + addresses
+        System.out.println(result); // employees.getCorrespondenceAddress().get() = cu .get() luam / returneaza valorile adreselor angajatilor
+        assertThat(result, sameBeanAs(1820L)); // optional se fol. pt creare instante / obiecte goale
     }
 
-    @Ignore
     @Test
     public void exercise_4_groupingBy_summingInt() {
         // find how much in total each company pays (annually) to their employees, order result by amount
@@ -69,7 +77,16 @@ public class Pack_4_Streams_Difficult {
         DecimalFormat decimalFormat = new DecimalFormat("£#,###.00");
         List<String> result = null;
 
-        //TODO write your code here
+        result = EMPLOYEES.stream()
+                .collect(groupingBy(company -> company.getCompany().getName(), // key - string - company
+                        summingInt(salary -> salary.getSalary().intValue()))) // value - int - salary
+                .entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()) // compare by value - salary
+                .map(s -> s.getKey() + " - " + decimalFormat.format(s.getValue()))
+                .collect(toList());
+
+        System.out.println(result);
 
         assertThat(result, sameBeanAs(asList(
                 "Anglo American - £12,119,153.00",
@@ -85,20 +102,28 @@ public class Pack_4_Streams_Difficult {
         )));
     }
 
-    @Ignore
     @Test
     public void exercise_5_patternCompileSplitAsStream() {
         // count the instances of words and output a list of formatted strings
         // output the strings sorted lexicographically by name
         // you can use collect twice
         // as always, a single statement solution is expected
-        // hint: look at Pattern.compile(regex).splitAsStream(string)
+        // hint: look at Pattern.compile(regex).splitAsStream(string) // split-uie String-ul dupa ce Pattern ii dau (regex)
 
         String string = "dog" + "\n" + "bird" + "\n" + "cat" + "\n" + "cat" + "\n" + "dog" + "\n" + "cat";
         List<String> result = null;
 
-        //TODO write your code here
+        result = Pattern.compile("\n").splitAsStream(string)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                // face un Map de perechi cu String-uri = keys = animale si Long = values = nr. animale;
+                // Collectors.groupingBy grupeaza cuv. // Collectors.counting() numara aparitiile fiecarui grup.
+                .entrySet() // fol. fct. entrySet() ca apoi sa folosim .Stream() == aplicam functiile de Stream, pt ca pe Map nu putem aplica fct.
+                .stream()
+                .sorted(Map.Entry.<String, Long>comparingByKey()) // comparingByKey()
+                .map(s -> s.getKey() + " - " + s.getValue())
+                .collect(toList());
 
+        System.out.println(result);
         assertThat(result, sameBeanAs(asList(
                 "bird - 1",
                 "cat - 3",
@@ -106,7 +131,7 @@ public class Pack_4_Streams_Difficult {
         )));
     }
 
-    @Ignore
+
     @Test
     public void exercise_6_mapToLong() {
         // the rows and columns of the chess board are assigned arbitrary numbers (instead of letters and digits)
@@ -117,12 +142,16 @@ public class Pack_4_Streams_Difficult {
         int[] columns = {6199, 9519, 6745, 8864, 8788, 7322, 7341, 7395};
         long result = 0;
 
-        //TODO write your code here
+        result = Arrays.stream(rows)
+                .flatMap(a -> Arrays.stream(columns).map(b -> a * b))
+                .mapToLong(n -> n)
+                .reduce(0, (a, b) -> a + b); // reduce afisarea calculului sumei produselor la un singur rezultat.
 
+        System.out.println(result);
         assertThat(result, sameBeanAs(4294973013L));
     }
 
-    @Ignore
+
     @Test
     public void exercise_7_randomLongs_concat_toArray() {
         // concatenate two random streams of numbers (seed is fixed for testing purposes),
@@ -134,8 +163,19 @@ public class Pack_4_Streams_Difficult {
         IntStream ints = new Random(0).ints(10);
         int[] result = null;
 
-        //TODO write your code here
-
+        result = LongStream.concat(longs, ints.asLongStream())// am convertit Stream de int la Long ca sa le putem concatena.
+                .boxed()
+                .map(n -> Math.abs(n)) // abs = absolute value, from negative to positive.
+                .sorted()
+                .skip(5)
+                .limit(10) // 10 middle elements picked.
+                .map(n -> n % 1000)
+                .mapToInt(x -> Math.toIntExact(x)) // nr. convertite in int
+                .toArray(); // si apoi colectate in Array
+        for (int i : result) {
+            System.out.println(i);
+        }
+        
         assertThat(result, sameBeanAs(new long[] {106, 266, 402, 858, 313, 688, 303, 137, 766, 896}));
     }
 
